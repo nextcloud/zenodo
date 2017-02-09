2445852e6bc8cfaef6a5028df9a12df0f46196a3
Maxence Lange <maxence@pontapreta.net>
2017-02-09 19:29:17 -0100

    better animation

--- js/dialog.js
+++ js/dialog.js
@@ -6,14 +6,14 @@ var zenodoDialog = {
 		$('#zendialog_imagetype').hide();
 
 		$('#zendialog_uploadtype').change(function () {
-			if ($('#zendialog_uploadtype option:selected').val() == "publication")
+			if ($('#zendialog_uploadtype option:selected').val() == 'publication')
 				$('#zendialog_imagetype').fadeOut(200, function () {
 					$('#zendialog_publicationtype').fadeIn(200);
 				});
 			else
 				$('#zendialog_publicationtype').fadeOut(200);
 
-			if ($('#zendialog_uploadtype option:selected').val() == "image")
+			if ($('#zendialog_uploadtype option:selected').val() == 'image')
 				$('#zendialog_publicationtype').fadeOut(200, function () {
 					$('#zendialog_imagetype').fadeIn(200);
 				});
@@ -21,8 +21,42 @@ var zenodoDialog = {
 				$('#zendialog_imagetype').fadeOut(200);
 		});
 
-		$('#zendialog_publicationdate').datepicker({ minDate: "-20Y", maxDate: "+20Y" });
-		$('#zendialog_embargodate').datepicker({ minDate: "-20Y", maxDate: "+20Y" });
+		$('#zendialog_publicationdate').datepicker({
+			dateFormat: "yy-mm-dd",
+			minDate: "-20Y",
+			maxDate: "+20Y"
+		});
+		$('#zendialog_embargodate').datepicker({
+			dateFormat: "yy-mm-dd",
+			minDate: "-20Y",
+			maxDate: "+20Y"
+		});
+
+		$('#zendialog_license').hide();
+		$('#zendialog_display_embargodate').hide();
+		$('#zendialog_display_accesscondition').hide();
+
+		$('#zendialog_accessright').change(function () {
+			if ($('#zendialog_accessright option:selected').val() == 'open')
+				$('#zendialog_license').fadeIn(200);
+			else
+				$('#zendialog_license').fadeOut(200);
+
+			if ($('#zendialog_accessright option:selected').val() == 'embargoed')
+				$('#zendialog_display_accesscondition').fadeOut(200, function () {
+					$('#zendialog_display_embargodate').fadeIn(200);
+				});
+			else
+				$('#zendialog_display_embargodate').fadeOut(200);
+
+			if ($('#zendialog_accessright option:selected').val() == 'restricted')
+				$('#zendialog_display_embargodate').fadeOut(200, function () {
+					$('#zendialog_display_accesscondition').fadeIn(200);
+				})
+			else
+				$('#zendialog_display_accesscondition').fadeOut(200);
+		});
+
 	},
 
 	metadata: function () {
@@ -37,9 +71,7 @@ var zenodoDialog = {
 			access_right: $('#zendialog_accessright option:selected').val(),
 			embargo_date: $('#zendialog_embargodate').val(),
 			access_conditions: $('#zendialog_accessconditions').val(),
-			license: $('#zendialog_license option:selected').val(),
-			doi: false,
-			prereserve_doi: false
+			license: $('#zendialog_license option:selected').val()
 		}
 
 		return data;
