c62da66dc64d641e130896fe388448293dda9f73
Maxence Lange <maxence@artificial-owl.com>
2018-08-17 16:59:39 +0200

    compat NC14

--- templates/settings.admin.php
+++ templates/settings.admin.php
@@ -28,34 +28,23 @@ style('zenodo', 'admin');
 
 ?>
 
-<fieldset id="zenodoAdminSettings" class="section">
-	<h2>
-		<img src="/apps/zenodo/img/zenodo.svg"
-			 style='vertical-align: baseline; margin-right: 25px;'>Research.
-		Shared.
-	</h2>
-	<table style="width: 650px;">
-		<tr class="zenodo_admin_head">
-			<td class="zenodo_admin_head" colspan="2">Zenodo Tokens</td>
-		</tr>
 
-		<tr>
-			<td class="zenodo_admin_left">Sandbox Token:</td>
-			<td><input type='text' style="width: 300px" name='sandboxtoken'
-					   id='sandboxtoken' original-title=''
-					   title='Access token for sandbox environment'></td>
-		</tr>
-		<tr>
-			<td class="zenodo_admin_left">Production Token:</td>
-			<td><input type='text' name='productiontoken' style="width: 300px"
-					   id='productiontoken' original-title=''
-					   title='Access token for production environment'></td>
-		</tr>
-		<tr>
-			<td></td>
-			<td><input type='submit' value='Store Zenodo Credentials'
-					   style="width: 250px" original-title='' style="width: 300px"
-					   id='tokensubmit' name='tokensubmit' title='Store Tokens'></input></td>
-		</tr>
-	</table>
-	<div id='clientstatus' style="font-size: .8em;"></div>
+
+
+
+<div class="section" id="circles">
+	<h2><?php p($l->t('Zenodo')) ?></h2>
+
+	<p>
+		<label><?php p($l->t('Sandbox Token')); ?></label><br />
+		<input type="text" id="sandboxtoken" />
+	</p>
+	<p>
+		<label><?php p($l->t('Production Token')); ?></label><br />
+		<input type="text" id="productiontoken" />
+	</p>
+
+	<p>
+		<input type="submit" id="tokensubmit" value="<?php p($l->t('Store Zenodo Credentials')); ?>">
+	</p>
+</div>
